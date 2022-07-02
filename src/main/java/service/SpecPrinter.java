5cb5e25fae7ad9d9a841ec75f4c981d0716db9c8
ASZhartun <78444904+ASZhartun@users.noreply.github.com>
2022-07-02 12:17:49 +0300

    completed SpecPrinter functionality. Adding calculate quantity column method for BillPrinter

--- src/main/java/service/SpecPrinter.java
+++ src/main/java/service/SpecPrinter.java
@@ -1,6 +1,7 @@
 package service;
 
 import dao.ReaderCSV.ReaderCSV;
+import entities.PositionBar;
 import entities.RebarMesh;
 import entities.Structure;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -49,35 +50,118 @@ public class SpecPrinter {
 
     /**
      * Склеивает строки, формирующиеся из полей объекта структуры, в единую таблицеподобную строку.
-     *
      * @param structure объект текущей структуры
      * @return строка (в физ. смысле таблица) из структуры
      */
     private String getBodyTableFrom(Structure structure) {
         final StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(createStructureTitle(structure));
-        stringBuilder.append(createStructureRebarMeshesTablePart(structure));
-        stringBuilder.append(createPositionBarsTablePart(structure));
-        stringBuilder.append(createConcreteTablePart(structure));
+        stringBuilder.append(createStructureTitle(structure)); //done
+        stringBuilder.append(createStructureRebarMeshesTablePart(structure)); //done
+        stringBuilder.append(createPositionBarsTablePart(structure)); //done
+        stringBuilder.append(createConcreteTablePart(structure)); //done
         return stringBuilder.toString();
     }
 
     /**
      * Создает табличный блок, описывающий все "Материалы". (В нашем случае марка бетона)
+     *
      * @param structure текущая структура
      * @return табличный блок в виде строки.
      */
     private String createConcreteTablePart(Structure structure) {
-        return null;
+        final StringBuilder materials = new StringBuilder();
+        materials.append(createTitleMaterialsBlock());
+        materials.append(createConcreteContent(structure));
+        return materials.toString();
+    }
+
+    /**
+     * Создает строчное представление бетона из объекта структуры.
+     * @param structure объект структуры
+     * @return строка-представление
+     */
+    private String createConcreteContent(Structure structure) {
+        final StringBuilder concreteContent = new StringBuilder();
+        concreteContent.append(" ;")
+                .append("СТБ EN 206-2016;")
+                .append(structure.getConcreteDefinition().get(0))
+                .append("; ; ;")
+                .append(structure.getConcreteVolume())
+                .append("м3;\n");
+        final ArrayList<String> concreteDefinition = structure.getConcreteDefinition();
+        for (int i = 1; i < concreteDefinition.size(); i++) {
+            concreteContent.append(" ; ;")
+                    .append(concreteDefinition.get(i))
+                    .append("; ; ; ;\n");
+        }
+        return concreteContent.toString();
+    }
+
+    /**
+     * Создает заголовок блока "Материалы", в которой размещается описание стержневых позиций.
+     *
+     * @return строка с заголовком
+     */
+    private String createTitleMaterialsBlock() {
+        final StringBuilder title = new StringBuilder();
+        title.append(" ;")
+                .append(" ;")
+                .append("Материалы;")
+                .append(" ;")
+                .append(" ;");
+        return title.toString();
     }
 
     /**
      * Создает табличный блок, описывающий все "Детали". (В нашем случае арматурные стержни)
+     *
      * @param structure текущая структура
      * @return табличный блок в виде строки.
      */
     private String createPositionBarsTablePart(Structure structure) {
-        return null;
+        final ArrayList<PositionBar> positions = structure.getPositions();
+        final StringBuilder positionBarBlock = new StringBuilder();
+        positionBarBlock.append(createTitlePositionBarsBlock());
+        for (PositionBar item :
+                positions) {
+            positionBarBlock.append(createPositionBarLine(item));
+        }
+        return positionBarBlock.toString();
+    }
+
+    /**
+     * Возвращает табличную строку на базе объекта стержневой позиции
+     *
+     * @param item объект стержневой позиции
+     * @return строка-представление
+     */
+    private String createPositionBarLine(PositionBar item) {
+        final StringBuilder line = new StringBuilder();
+        line.append(" ;")
+                .append(" ;")
+                .append(item.toString())
+                .append(";")
+                .append(item.getQuantity())
+                .append(";")
+                .append(item.getWeight())
+                .append(";")
+                .append(" ;\n");
+        return line.toString();
+    }
+
+    /**
+     * Создает заголовок блока "Детали", в которой размещается описание стержневых позиций.
+     *
+     * @return строка с заголовком
+     */
+    private String createTitlePositionBarsBlock() {
+        final StringBuilder title = new StringBuilder();
+        title.append(" ;")
+                .append(" ;")
+                .append("Детали;")
+                .append(" ;")
+                .append(" ;");
+        return title.toString();
     }
 
     /**
@@ -118,11 +202,11 @@ public class SpecPrinter {
         final StringBuilder line = new StringBuilder(" ");
         line.append(" ;")
                 .append(rebarMesh.getDoc())
-                .append(" ;")
+                .append(";")
                 .append(rebarMesh.getStartNaming())
-                .append(" ;")
+                .append(";")
                 .append(rebarMesh.getQuantity())
-                .append(" ;")
+                .append(";")
                 .append(rebarMesh.getCross().getWeight() + rebarMesh.getBase().getWeight())
                 .append(" ;\n");
         return line.toString();
