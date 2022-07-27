a6395246d9fcd6a80a817bb955c3436bb1705a4c
ASZhartun <as.zhartun@gmail.com>
2022-07-27 08:58:49 +0300

    add beans for future gui listeners

--- src/main/java/service/Director.java
+++ src/main/java/service/Director.java
@@ -6,26 +6,19 @@ import config.ServiceConfiguration;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.base.Operator;
 import service.extra.Librarian;
-
-
-import java.net.URL;
 
 public class Director extends Application {
     public static final String APP_TITLE = "Расчет спецификации 1.0";
@@ -42,6 +35,17 @@ public class Director extends Application {
     }
 
     @Override
+    public void init() throws Exception {
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(EntityConfiguration.class);
+        context.register(ServiceConfiguration.class);
+        context.refresh();
+        this.operator = (Operator) context.getBean("operator");
+        this.librarian = (Librarian) context.getBean("librarian");
+        super.init();
+    }
+
+    @Override
     public void start(Stage stage) throws Exception {
         // Заголовк программы
         final Label title = new Label(APP_TITLE);
@@ -49,7 +53,7 @@ public class Director extends Application {
         final HBox header = new HBox();
         header.getChildren().addAll(title);
         header.setAlignment(Pos.CENTER);
-        header.setPadding(new Insets(20,20,20,20));
+        header.setPadding(new Insets(20, 20, 20, 20));
 
         // Основаная часть интерфейса программы
         final GridPane main = new GridPane();
@@ -72,7 +76,7 @@ public class Director extends Application {
         librarianBlock.setVgap(SMALL_GAP);
         final HBox libreTitleBlock = new HBox(libreTitle);
         libreTitleBlock.setAlignment(Pos.CENTER);
-        librarianBlock.setPadding(new Insets(10,10,10,10));
+        librarianBlock.setPadding(new Insets(10, 10, 10, 10));
         // таблица с дополнительными КЖ.И
         final ObservableList<String> rebarMeshes = FXCollections.observableArrayList("Каркас КР1", "Каркас КР2");
         final TableView<String> viewRebarMeshes = new TableView<>(rebarMeshes);
@@ -85,7 +89,7 @@ public class Director extends Application {
         final Button addRCP = new Button("Add RCP");
         final HBox addingRCPBlock = new HBox(newItem, addRCP);
         addingRCPBlock.setSpacing(SMALL_GAP);
-        addingRCPBlock.setPadding(new Insets(SMALL_GAP, SMALL_GAP,SMALL_GAP,SMALL_GAP));
+        addingRCPBlock.setPadding(new Insets(SMALL_GAP, SMALL_GAP, SMALL_GAP, SMALL_GAP));
         addingRCPBlock.setAlignment(Pos.CENTER);
         // поле с кнопкой изменения имени выделенной в таблице КЖ.И, с кнопкой удаление выделенной в таблице КЖ.И
         final TextField name = new TextField();
@@ -94,32 +98,32 @@ public class Director extends Application {
         final HBox updatingRCPBlock = new HBox(name, edit, delete);
         updatingRCPBlock.setAlignment(Pos.CENTER);
         updatingRCPBlock.setSpacing(SMALL_GAP);
-        updatingRCPBlock.setPadding(new Insets(SMALL_GAP,SMALL_GAP,SMALL_GAP,SMALL_GAP));
+        updatingRCPBlock.setPadding(new Insets(SMALL_GAP, SMALL_GAP, SMALL_GAP, SMALL_GAP));
 
-        librarianBlock.add(libreTitleBlock,0,0);
-        librarianBlock.add(viewRebarMeshes,0,1);
-        librarianBlock.add(addingRCPBlock,0,2);
-        librarianBlock.add(updatingRCPBlock,0,3);
+        librarianBlock.add(libreTitleBlock, 0, 0);
+        librarianBlock.add(viewRebarMeshes, 0, 1);
+        librarianBlock.add(addingRCPBlock, 0, 2);
+        librarianBlock.add(updatingRCPBlock, 0, 3);
 
         // блок с подписью
-        final Label sign = new Label("Анатолий Жартун, 2022");
+        final Label sign = new Label("as.zhartun@gmail.com, 2022");
         final HBox footer = new HBox(sign);
         footer.setAlignment(Pos.CENTER);
-        footer.setPadding(new Insets(20,20,20,20));
+        footer.setPadding(new Insets(20, 20, 20, 20));
         sign.setAlignment(Pos.CENTER);
 
-        main.add(choseFileBlock,0,0);
-        main.add(runBlock,0,1);
-        main.add(librarianBlock,0,2);
-        main.setPadding(new Insets(SMALL_GAP,SMALL_GAP,SMALL_GAP,SMALL_GAP));
+        main.add(choseFileBlock, 0, 0);
+        main.add(runBlock, 0, 1);
+        main.add(librarianBlock, 0, 2);
+        main.setPadding(new Insets(SMALL_GAP, SMALL_GAP, SMALL_GAP, SMALL_GAP));
         main.setVgap(SMALL_GAP);
 
         final GridPane body = new GridPane();
 //        body.setGridLinesVisible(true);
 
-        body.add(header, 0,0);
-        body.add(main, 0,1);
-        body.add(footer,0,2);
+        body.add(header, 0, 0);
+        body.add(main, 0, 1);
+        body.add(footer, 0, 2);
 //        body.setPadding(new Insets(200,-SMALL_GAP0,SMALL_GAP0,SMALL_GAP0));
         body.setAlignment(Pos.CENTER);
         body.prefHeight(600);
