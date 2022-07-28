4ba13f9857546df9c98b656a158ab7b562f30c6b
ASZhartun <as.zhartun@gmail.com>
2022-07-28 17:13:38 +0300

    add eventListener for lib_add_btn

--- src/main/java/service/Director.java
+++ src/main/java/service/Director.java
@@ -7,6 +7,7 @@ import entities.RebarCage;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -19,6 +20,7 @@ import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import javafx.stage.Window;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import service.base.Operator;
@@ -26,12 +28,16 @@ import service.extra.Librarian;
 
 import java.io.File;
 import java.net.URL;
-import java.util.HashSet;
 
 public class Director extends Application {
 
+    //TODO: separate business logic and JavaFx Controller
+
     private Operator operator;
     private Librarian librarian;
+
+    @FXML
+    private Button addRPCButton;
 
     public static void main(String[] args) {
         launch(args);
@@ -58,12 +64,26 @@ public class Director extends Application {
         URL xmlUrl = getClass().getResource("/gui/main.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
-
         stage.setScene(new Scene(root));
         stage.show();
-
     }
 
+    public void addRPC() {
+        final Window window = addRPCButton.getScene().getWindow();
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Выберите csv файл со спецификацией");
+        if (window != null) {
+            final File file = fileChooser.showOpenDialog(window);
+            librarian.addNewRebarCageFrom(file.getPath());
+            addRPCButton.setText("Ладное");
+        } else {
+            addRPCButton.setText("Неладное");
+        }
+    }
+
+    public void deleteRPC() {
+
+    }
 
     @Autowired
     public void setOperator(Operator operator) {
